b940132a8989c40f77d3af2e1e33a5722e51eee1
lipika1911 <lipikaarya1723@gmail.com>
2025-07-28 18:44:19 +0530

    feat: setup routes, defined page paths, and added base components

---
+++ client/src/pages/GenerateImages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const GenerateImages = () => {
+  return (
+    <div>GenerateImages</div>
+  )
+}
+
+export default GenerateImages
+
---
+++ client/src/pages/RemoveBackground.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>RemoveBackground</div>
+  )
+}
+
+export default RemoveBackground
+
+
+
+
